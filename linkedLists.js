92bb729078960d567dbab4348b8675008a875055
joan-kii <joan.sb81@gmail.com>
2023-03-21 05:40:29 +0100

    add linkedList methods

--- linkedLists.js
+++ linkedLists.js
@@ -1,31 +1,82 @@
 class LinkedList {
-  constructor(nodes = new Node()) {
-    this.nodes = nodes;
+  #head;
+
+  constructor(head = new Node()) {
+    this.#head = head;
   }
 
   append(value) {
     // Add node with value as last node
-    // Seguir aquí (for node in this.nodes)
+    let node = this.#head;
+
+    while (node) {
+      if (node.nextNode === null) {
+        node.nextNode = new Node(value);
+        break;
+      } else {
+        node = node.nextNode;
+      }
+    }
   }
 
   prepend(value) {
     // Add node with value as first node
+    let node = this.#head;
+    this.#head = new Node(value, node);
   }
 
   getSize() {
     // Return the linked list size
+    let count = 0;
+    let node = this.#head;
+
+    while (node) {
+      if (node.nextNode === null) {
+        count++;
+        break;
+      } else {
+        count++;
+        node = node.nextNode;
+      }
+    }
+
+    return count;
   }
 
-  head() {
+  get size() {
+    return this.getSize();
+  }
+
+  get head() {
     // Return the first node
+    return this.#head;
   }
 
-  tail() {
+  get tail() {
     // Return the last node
+    let last = this.#head;
+
+    if (last) {
+      while (last.nextNode) {
+        last = last.nextNode;
+      }
+    }
+
+    return last;
   }
 
   at(index) {
     // Return the node at index
+    let count = 0;
+    let node = this.#head;
+
+    while (node) {
+      if (count === index) return node;
+      count++;
+      node = node.nextNode;
+    }
+
+    return 'There is not a node in that index';
   }
 
   pop() {
@@ -51,23 +102,30 @@ class LinkedList {
   removeAt(index) {
     // Remove node at index
   }
-
-  get size() {
-    return this.getSize();
-  }
 }
 
 class Node {
+  #value;
   constructor(value = null, nextNode = null) {
-    this.value = value;
+    this.#value = value;
     this.nextNode = nextNode;
+  }
+
+  get value() {
+    return this.#value;
   }
 }
 
 
-const node = new Node('Ter Stegen');
+const node = new Node('Balde');
 
 const barça = new LinkedList(node);
 
-console.log(node);
-console.log(barça);
+barça.append('Araujo');
+barça.prepend('Ter Stegen');
+
+// console.log(barça.size);
+// console.log(barça.head);
+// console.log(barça.tail);
+// console.log(barça.at(2));
+// console.log(barça.at(3));
