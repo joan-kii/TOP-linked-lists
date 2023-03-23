eaa5555a9819147a9237b83abf8541b4d0a55614
Joan Serrano <59319203+joan-kii@users.noreply.github.com>
2023-03-23 05:11:30 +0100

    last commit

--- linkedLists.js
+++ linkedLists.js
@@ -10,12 +10,11 @@ class LinkedList {
     let node = this.#head;
 
     while (node) {
-      if (node.nextNode === null) {
+      if (node.nextNode === null) { 
         node.nextNode = new Node(value);
         break;
-      } else {
-        node = node.nextNode;
       }
+      node = node.nextNode;
     }
   }
 
@@ -25,26 +24,17 @@ class LinkedList {
     this.#head = new Node(value, node);
   }
 
-  getSize() {
+  get size() {
     // Return the linked list size
     let count = 0;
     let node = this.#head;
 
     while (node) {
-      if (node.nextNode === null) {
-        count++;
-        break;
-      } else {
-        count++;
-        node = node.nextNode;
-      }
+      count++;
+      node = node.nextNode;
     }
 
     return count;
-  }
-
-  get size() {
-    return this.getSize();
   }
 
   get head() {
@@ -76,31 +66,96 @@ class LinkedList {
       node = node.nextNode;
     }
 
-    return 'There is not a node in that index';
+    return null;
   }
 
   pop() {
     // Remove the last node
-  }
+    let node = this.#head;
 
-  contains() {
+    while (node) {
+      if (node.nextNode.nextNode === null) node.nextNode = null;
+      node = node.nextNode;
+    }
+  } 
+
+  contains(value) {
     // Return true/false if the value exists
+    let node = this.#head;
+
+    while(node) {
+      if (node.value === value) return true;
+      node = node.nextNode;
+    }
+
+    return false;
   }
 
   find(value) {
     // Return index or null of the node if exists
+    let node = this.#head;
+    let count = 0;
+
+    while(node) {
+      if (node.value === value) return count;
+      node = node.nextNode;
+      count += 1;
+    }
+
+    return null;
   }
 
   toString() {
     // Return the LinkedList as a string
+    let node = this.#head;
+    let output = '';
+
+    while(node) {
+      output += `( ${node.value} ) -> `;
+      node = node.nextNode;
+    }
+
+    return output + '( null )';
   }
 
   insertAt(value, index) {
     // Insert node with value at index
+    let node = this.#head;
+    let count = 0;
+    let prev;
+
+    while (node) {
+      if (count === index) {
+        
+        prev.nextNode = new Node(value, node);
+        break;
+      }
+      
+      if (count > 0) prev = node;
+
+      node = node.nextNode;
+      count += 1;
+    }
   }
 
   removeAt(index) {
     // Remove node at index
+    let node = this.#head;
+    let count = 0;
+    let prev;
+
+    while (node) {
+      if (count === index) {
+        
+        prev.nextNode = node.nextNode;
+        break;
+      }
+      
+      if (count > 0) prev = node;
+
+      node = node.nextNode;
+      count += 1;
+    }
   }
 }
 
@@ -122,10 +177,32 @@ const node = new Node('Balde');
 const barça = new LinkedList(node);
 
 barça.append('Araujo');
+barça.append('Christensen');
+barça.append('Kounde');
+barça.append('Pedri');
+barça.append('Busquets');
+barça.append('De Jong');
+barça.append('Gavi');
+barça.append('Lewandowski');
+barça.append('Raphinha');
 barça.prepend('Ter Stegen');
 
-// console.log(barça.size);
-// console.log(barça.head);
-// console.log(barça.tail);
-// console.log(barça.at(2));
-// console.log(barça.at(3));
+// console.log(barça.size); // 11
+// console.log(barça.head.value); // Ter Stegen
+// console.log(barça.tail.value); // Raphinha
+// console.log(barça.at(6).value); // Busquets
+// barça.append('Messi');
+// console.log(barça.size); // 12
+// barça.pop();
+// console.log(barça.size); // 11
+// console.log(barça.contains('Gavi')); // True
+// console.log(barça.contains('Romario')); // False
+// console.log(barça.find('Pedri')); // 5
+// console.log(barça.find('Romario')); // Null
+// console.log(barça.toString()); // ( Ter Stegen ) -> ( Balde ) -> ( Araujo ) -> ( Christensen ) -> ( Kounde ) -> ( Pedri ) -> ( Busquets ) -> ( De Jong ) -> ( Gavi ) -> ( Lewandowski ) -> ( Raphinha ) -> ( null )
+// barça.insertAt('Ronaldinho', 9);
+// console.log(barça.size); // 12
+// console.log(barça.toString()); // ( Ter Stegen ) -> ( Balde ) -> ( Araujo ) -> ( Christensen ) -> ( Kounde ) -> ( Pedri ) -> ( Busquets ) -> ( De Jong ) -> ( Gavi ) -> ( Lewandowski ) -> ( Ronaldinho ) -> ( Raphinha ) -> ( null )
+// barça.removeAt(9);
+// console.log(barça.size); // 11
+
